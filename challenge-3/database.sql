1173153c1665c9740f326b607ff38799d0aa0f5d
Henry Caldwell <henrycaldwell2005@gmail.com>
2025-03-30 12:47:59 -0400

    wrote query

--- challenge-3/database.sql
+++ challenge-3/database.sql
@@ -4,3 +4,16 @@ create table logins (
     login_date DATE NOT NULL,
     PRIMARY KEY (user_id, login_date)
 );
+
+SELECT
+  (100.0 * COUNT(DISTINCT l1.user_id) / COUNT(DISTINCT l0.user_id)) AS percent_returned_within_7_days
+FROM (
+    SELECT user_id, MIN(login_date) AS first_login
+    FROM logins
+    GROUP BY user_id
+) l0
+LEFT JOIN logins l1
+  ON l0.user_id = l1.user_id
+  AND l1.login_date > l0.first_login
+  AND l1.login_date <= DATE_ADD(l0.first_login, INTERVAL 7 DAY)
+WHERE l1.login_date IS NOT NULL;
