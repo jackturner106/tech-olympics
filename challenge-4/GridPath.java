50d813fb009a1ea8c8f38d2d851b84be9d0dc66b
Jack Turner <redjackt@outlook.com>
2025-03-30 13:14:24 -0400

    question 4

--- challenge-4/GridPath.java
+++ challenge-4/GridPath.java
@@ -9,23 +9,31 @@ private int[][] grid;
 * row and col do not specify the element in the last row and last column of grid.
 */
 public Location getNextLoc(int row, int col) { 
-    if (row == this.grid.length()) {
+    if (row == this.grid.length) {
         return new Location(row, col + 1);
-    } else if (col == this.grid[row].length()) {
+    } else if (col == this.grid[row].length) {
         return new Location(row + 1, col);
-    } else if this.grid[row][col + 1] < this.grid[row][col] {
+    } else if (this.grid[row][col + 1] < this.grid[row][col]) {
         return new Location(row, col + 1);
     } else {
         return new Location(row + 1, col);
     }
 }
+
 /**
 * Computes and returns the sum of all values on a path through grid, as described in
 * part (b)
 * Preconditions: row is a valid row index and col is a valid column index in grid.
 * row and col do not specify the element in the last row and last column of grid.
 */
-public int sumPath(int row, int col)
-{ /* to be implemented in part (b) */ }
+public int sumPath(int row, int col) { 
+    Location loc = new Location(row, col);
+    int sum = this.grid[row][col];
+    while (!(loc.getRow() == this.grid.length && loc.getCol() == this.grid[loc.getRow()].length)) {
+        loc = this.getNextLoc(loc.getRow(), loc.getCol());
+        sum = sum + this.grid[loc.getRow()][loc.getCol()];
+    }
+    return sum;
+}
 // There may be instance variables, constructors, and methods that are not shown.
 }
