095cd83ec4ff55b99c6838f405a053faa57edf2b
Jack Turner <redjackt@outlook.com>
2025-03-30 14:58:14 -0400

    b

--- challenge-11/c11.py
+++ challenge-11/c11.py
@@ -18,14 +18,18 @@ with open(filename) as f:
 
 CUSTOMER_ID_IND = data[0].index("CustomerID")
 TRANSACTION_AMOUNT_IND = data[0].index("TransactionAmount")
-
+od = data[1:]
 # step 1: filter:
 data = [d for d in data[1:] if len(d) == 4]
 data = [d for d in data if float(d[TRANSACTION_AMOUNT_IND]) > 100]
 
 for d in data:
     unique_whales.add(d[CUSTOMER_ID_IND])
-    sum += float(d[TRANSACTION_AMOUNT_IND])
 
 print("Unique Customers: {}".format(len(unique_whales)))
+
+sum = 0
+for d in od:
+    if d[CUSTOMER_ID_IND] in unique_whales:
+        sum += float(d[TRANSACTION_AMOUNT_IND])
 print("Total Money Spent: {}".format(sum))
